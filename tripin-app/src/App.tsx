fbe721ad8c07e8ecfddff376aba92e276d642e3b
alfrzaa <egydiaalfariza74@gmail.com>
2024-11-17 21:50:21 +0700

    ubah pages

--- tripin-app/src/App.tsx
+++ tripin-app/src/App.tsx
@@ -1,11 +1,7 @@
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TermsAndConditions from "./components/TermsAndCondition";
-import Dashboard from "./components/dashboard";
-import PrivacyPolicy from "./components/PrivacyPolicy";
-import LoginTes from "./components/LoginTes";
-// import Tes from "./components/tes";
-
+import TermsAndConditions from "./pages/TermsAndCondition";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 function App() {
   // return <Tes />
@@ -15,7 +11,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/TermsAndCondition" element={<TermsAndConditions />} />
         <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   )
